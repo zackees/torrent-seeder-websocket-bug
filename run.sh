c0f81cf76b75914ddfc7d5028449fd6a6958482e
zackees <z@zackees.com>
2022-07-24 02:28:07 -0700

    update

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
+echo running webtorrent-hybrid seed beavis_and_butthead.mp4 --keep-seeding --announce wss://webtorrent-tracker.onrender.com --port 80
 webtorrent-hybrid seed beavis_and_butthead.mp4 --keep-seeding --announce wss://webtorrent-tracker.onrender.com --port 80
